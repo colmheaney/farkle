41da722fa49ed244ca50cc72f028124a04b4dae4
Colm Heaney <colmheaney@voxbit.com>
2015-06-08 21:28:36 +0100

    changing port number

--- server.js
+++ server.js
@@ -112,7 +112,6 @@ io.on('connection', function(socket){
         
 });
 
-
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(8081, '127.0.0.1', function(){
+  console.log('listening on 127.0.0.1:8081');
 });
